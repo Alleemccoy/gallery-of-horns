13586be480cdd7c3d88e1ade6a4c79ea74114626
alleemccoy <alleemccoy@hotmail.com>
2021-03-20 17:41:03 -0700

    Completed header and minimal credit to submit, will continue later to finish assignment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 class App extends React.Component{
   render(){
     return(
       <div className="">
-        <header>
-
-          <h1>Hello World</h1>);
-        </header>
+        <Header />
+        {/* main & footer create the same way we created Header */}
 
       </div>
+    );
   }
 }
 
