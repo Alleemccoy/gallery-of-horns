7583e0b9b7ba82a129c9b7e5d206ef526e75e6de
alleemccoy <alleemccoy@hotmail.com>
2021-03-22 19:59:03 -0700

    rearranged what was in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './App.css';
 import Header from './Header';
-import Footer from './Footer';
 import Main from './Main';
+import Footer from './Footer';
+
+import './App.css';
 
 class App extends React.Component{
   render(){
