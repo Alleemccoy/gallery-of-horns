8410ca27c7cc4564bc5cb12a02092a075531b080
alleemccoy <alleemccoy@hotmail.com>
2021-03-22 20:28:37 -0700

    Added to Main.js and created HornedBeasts.css and HornedBeasts.js files

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Footer from './Footer';
 
 import './App.css';
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    return (
       <div className="">
         <Header />
-        <Footer />
         <Main />
+        <Footer />
 
       </div>
     );
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class Header extends React.Component{
+class Header extends React.Component {
 
-  render(){
-    return(<h1>Hi everyone!</h1>)
+  render() {
+    return (<header><h1>Hi everyone!</h1></header>)
   }
 }
 
